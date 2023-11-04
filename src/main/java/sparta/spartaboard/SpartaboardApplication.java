e028ae2d504d09f5708b9d5b65f8a70c18e5acd7
barami62 <wodbs7758@naver.com>
2023-11-04 19:41:56 +0900

    init commit

---
+++ src/main/java/sparta/spartaboard/SpartaboardApplication.java
@@ -0,0 +1,13 @@
+package sparta.spartaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartaboardApplication.class, args);
+	}
+
+}

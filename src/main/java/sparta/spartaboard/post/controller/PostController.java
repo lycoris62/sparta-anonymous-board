b1da52c9ab032ff4e48a938c3d730d586f660800
barami62 <wodbs7758@naver.com>
2023-11-04 23:36:51 +0900

    feat : 게시글 조회 컨트롤러 API 추가

--- src/main/java/sparta/spartaboard/post/controller/PostController.java
+++ src/main/java/sparta/spartaboard/post/controller/PostController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import sparta.spartaboard.post.dto.PostDetailResponseDto;
 import sparta.spartaboard.post.dto.PostPreviewResponseDto;
 import sparta.spartaboard.post.service.PostService;
 
@@ -23,5 +25,12 @@ public class PostController {
 		List<PostPreviewResponseDto> posts = postService.getPosts();
 
 		return ResponseEntity.ok(posts);
+	}
+
+	@GetMapping("/posts/{postId}")
+	public ResponseEntity<PostDetailResponseDto> getPost(@PathVariable Long postId) {
+		PostDetailResponseDto postDetail = postService.getPost(postId);
+
+		return ResponseEntity.ok(postDetail);
 	}
 }

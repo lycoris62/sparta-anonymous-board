80cca86942c21d18544571afbe021c6af8c967ff
barami62 <wodbs7758@naver.com>
2023-11-04 23:29:17 +0900

    feat : 게시글 목록 조회 서비스 기능 추가

--- src/main/java/sparta/spartaboard/post/service/PostService.java
+++ src/main/java/sparta/spartaboard/post/service/PostService.java
@@ -1,9 +1,12 @@
 package sparta.spartaboard.post.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import sparta.spartaboard.post.dto.PostPreviewResponseDto;
 import sparta.spartaboard.post.repository.PostRepository;
 
 @Service
@@ -13,5 +16,10 @@ public class PostService {
 
 	private final PostRepository postRepository;
 
-
+	public List<PostPreviewResponseDto> getPosts() {
+		return postRepository.findAllByOrderByCreatedAtDesc()
+			.stream()
+			.map(PostPreviewResponseDto::new)
+			.toList();
+	}
 }

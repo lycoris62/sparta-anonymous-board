9b175c0a946c1ffdb6e781bb83d661b18eb158f1
barami62 <wodbs7758@naver.com>
2023-11-05 17:26:03 +0900

    feat : 요청 DTO에 입력 길이 충족 조건 추가

--- src/main/java/sparta/spartaboard/domain/post/dto/request/PostCreateRequestDto.java
+++ src/main/java/sparta/spartaboard/domain/post/dto/request/PostCreateRequestDto.java
@@ -1,13 +1,26 @@
 package sparta.spartaboard.domain.post.dto.request;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class PostCreateRequestDto {
 
+	@NotBlank
+	@Length(min = 1, max = 30)
 	private String title;
+
+	@NotBlank
+	@Length(min = 2, max = 20)
 	private String author;
+
+	@NotBlank
+	@Length(min = 4, max = 50)
 	private String password;
+
+	@NotBlank
 	private String contents;
 
 	public void changeEncodedPassword(String encodedPassword) {
--- src/main/java/sparta/spartaboard/domain/post/dto/request/PostEditRequestDto.java
+++ src/main/java/sparta/spartaboard/domain/post/dto/request/PostEditRequestDto.java
@@ -1,11 +1,21 @@
 package sparta.spartaboard.domain.post.dto.request;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class PostEditRequestDto {
 
+	@NotBlank
+	@Length(min = 1, max = 30)
 	private String title;
+
+	@NotBlank
+	@Length(min = 2, max = 20)
 	private String author;
+
+	@NotBlank
 	private String contents;
 }

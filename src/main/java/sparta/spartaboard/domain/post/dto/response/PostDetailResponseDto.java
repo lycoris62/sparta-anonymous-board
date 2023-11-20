21c46883e9131a2b3ab5fc2bf50c60fc00c01525
barami62 <wodbs7758@naver.com>
2023-11-21 00:37:28 +0900

    feat : mapper 에 쓰일 Setter 추가

--- src/main/java/sparta/spartaboard/domain/post/dto/response/PostDetailResponseDto.java
+++ src/main/java/sparta/spartaboard/domain/post/dto/response/PostDetailResponseDto.java
@@ -3,28 +3,20 @@ package sparta.spartaboard.domain.post.dto.response;
 import java.time.LocalDateTime;
 
 import lombok.Getter;
-import sparta.spartaboard.domain.post.entity.Post;
+import lombok.Setter;
 
 /**
  * 게시글 세부 정보를 응답하는 DTO
  * 비밀번호는 들어가면 안 된다.
  */
 @Getter
+@Setter
 public class PostDetailResponseDto {
 
-	private final Long id;
-	private final String title;
-	private final String author;
-	private final String contents;
-	private final LocalDateTime createdAt;
-	private final LocalDateTime modifiedAt;
-
-	public PostDetailResponseDto(Post post) {
-		this.id = post.getId();
-		this.title = post.getTitle();
-		this.author = post.getAuthor();
-		this.contents = post.getContents();
-		this.createdAt = post.getCreatedAt();
-		this.modifiedAt = post.getModifiedAt();
-	}
+	private Long id;
+	private String title;
+	private String author;
+	private String contents;
+	private LocalDateTime createdAt;
+	private LocalDateTime modifiedAt;
 }
--- src/main/java/sparta/spartaboard/domain/post/dto/response/PostPreviewResponseDto.java
+++ src/main/java/sparta/spartaboard/domain/post/dto/response/PostPreviewResponseDto.java
@@ -3,24 +3,18 @@ package sparta.spartaboard.domain.post.dto.response;
 import java.time.LocalDateTime;
 
 import lombok.Getter;
-import sparta.spartaboard.domain.post.entity.Post;
+import lombok.Setter;
 
 /**
  * 게시글 목록 반환을 위한 간소화된 게시글 정보 DTO
  * 비밀번호는 들어가면 안 된다.
  */
 @Getter
+@Setter
 public class PostPreviewResponseDto {
 
-	private final Long id;
-	private final String title;
-	private final String author;
-	private final LocalDateTime createdAt;
-
-	public PostPreviewResponseDto(Post post) {
-		this.id = post.getId();
-		this.title = post.getTitle();
-		this.author = post.getAuthor();
-		this.createdAt = post.getCreatedAt();
-	}
+	private Long id;
+	private String title;
+	private String author;
+	private LocalDateTime createdAt;
 }
